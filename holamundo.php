abf0ccbc82e1f347244862a64a4ce29683b360c2
angelbarba24 <abarfer0206@g.educaand.es>
2025-09-16 19:58:00 +0200

    Primera subida drag & drop

---
+++ holamundo.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Relación 1 - Hola mundo</title>
+    <link rel="shortcut icon" href="./playamar.png" type="image/x-icon">
+</head>
+<body>
+    <h1>Hola mundo en php</h1>
+    <?php
+        echo "<h3 style='color: red;'>Hola mundo</h3>"
+    ?>
+</body>
+</html>
